d553e10b1d30916479b52a7d2b41a5e342a1d28c
eatonemmerich <e.e.metabee@gmail.com>
2014-09-24 02:48:18 +0200

    trying to run a GPU example

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: eaton
+ *
+ * Created on September 23, 2014, 11:08 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
